c59550691a8ea009d6adff446a2275a717cb20c5
Raitoforce34 <mahgt_34@hotmail.com>
2021-04-03 12:49:51 -0600

    view generate excels update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import * as Chartist from 'chartist';
-import {BoxesService} from '../../services/boxes.service';
-import {ReportService} from '../../services/report.service';
-import Swal from 'sweetalert2';
-import MessagesUtil from '../../util/messages.utill';
-import {ERROR_MESSAGE, SECOND} from '../../util/Config.utils';
-import {Router} from '@angular/router';
+import {messageErrorValidation} from '../../util/ValidatorsHelper';
+import {Observable} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {PoliticalService} from '../../services/political.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,189 +10,37 @@ import {Router} from '@angular/router';
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-    drawChartStrucutre = false;
+    form: any;
+    parties$: Observable<any>;
+    excelTypes = [
+        {id: 1, name: 'Diputados DRP'},
+        {id: 2, name: 'Diputados DMR'},
+        {id: 3, name: 'Diputados y Presidentes para INE'},
+        {id: 4, name: 'Sindicaturas y Regidurias para INE'},
+    ];
 
     constructor(
-        private _boxes: BoxesService,
-        private _report: ReportService,
-        private _router: Router
+        private politicalService: PoliticalService
     ) {
-    }
-
-    startAnimationForLineChart(chart) {
-        let seq: any, delays: any, durations: any;
-        seq = 0;
-        delays = 80;
-        durations = 500;
-
-        chart.on('draw', function (data) {
-            if (data.type === 'line' || data.type === 'area') {
-                data.element.animate({
-                    d: {
-                        begin: 600,
-                        dur: 700,
-                        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-                        to: data.path.clone().stringify(),
-                        easing: Chartist.Svg.Easing.easeOutQuint
-                    }
-                });
-            } else if (data.type === 'point') {
-                seq++;
-                data.element.animate({
-                    opacity: {
-                        begin: seq * delays,
-                        dur: durations,
-                        from: 0,
-                        to: 1,
-                        easing: 'ease'
-                    }
-                });
+        this.form = new FormGroup({
+                party: new FormControl(null, Validators.required),
+                type: new FormControl(null, Validators.required),
             }
-        });
-        seq = 0;
-    };
-
-    startAnimationForBarChart(chart) {
-        let seq2: any, delays2: any, durations2: any;
-
-        seq2 = 0;
-        delays2 = 80;
-        durations2 = 500;
-        chart.on('draw', function (data) {
-            if (data.type === 'bar') {
-                seq2++;
-                data.element.animate({
-                    opacity: {
-                        begin: seq2 * delays2,
-                        dur: durations2,
-                        from: 0,
-                        to: 1,
-                        easing: 'ease'
-                    }
-                });
-            }
-        });
-        seq2 = 0;
-    };
-
-    ngOnInit() {
-        // Swal.showLoading();
-    }
-
-    setGraphics() {
-        console.log('Refresco gráficas');
-        if (this._router['url'] === '/dashboard') {
-            this._report.getGraphics().subscribe(
-                response => {
-                    console.log('GRAPHICS --> ', response);
-                    Swal.close();
-                    this.setGraphicsCallback(response['data']);
-                },
-                error => MessagesUtil.errorMessage(ERROR_MESSAGE),
-            );
-        }
-    }
-
-    setGraphicsCallback(data) {
-
-        this.startAnimationForLineChart(new Chartist.Line('#dailySalesChart',
-            {
-                labels: data.week.label,
-                series: [
-                    data.week.value
-                ]
-            },
-            {
-                lineSmooth: Chartist.Interpolation.cardinal({
-                    tension: 0
-                }),
-                low: 0,
-                high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-                chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
-            })
         );
 
-        /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+        this.parties$ = this.politicalService.getAll();
+    }
 
-        this.startAnimationForBarChart(new Chartist.Line('#websiteViewsChart',
-            {
-                labels: data.district.label,
-                series: [
-                    data.district.value
-                ]
-            },
-            {
-                lineSmooth: Chartist.Interpolation.cardinal({
-                    tension: 0
-                }),
-                low: 0,
-                high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-                chartPadding: {top: 0, right: 0, bottom: 0, left: 0}
-            }
-        ));
-
-        // start animation for the Completed Tasks Chart - Line Chart
-        this.startAnimationForLineChart(new Chartist.Bar('#completedTasksChart', {
-            labels: data.month.label,
-            series: [
-                data.month.value
-            ]
-        }, {
-            axisX: {
-                showGrid: false
-            },
-            low: 0,
-            high: 1000,
-            chartPadding: {top: 0, right: 5, bottom: 0, left: 0}
-        }, [
-            ['screen and (max-width: 640px)', {
-                seriesBarDistance: 5,
-                axisX: {
-                    labelInterpolationFnc: function (value) {
-                        return value[0];
-                    }
-                }
-            }]
-        ]));
-
-        this.startAnimationForLineChart(new Chartist.Line('#dayChart',
-            {
-                labels: data.hour.label,
-                series: [
-                    data.hour.value
-                ]
-            },
-            {
-                lineSmooth: Chartist.Interpolation.cardinal({
-                    tension: 0
-                }),
-                low: 0,
-                high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-                chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
-            }
-        ));
-
-        if (data.structure && data.structure.value[0] != 0) {
-            this.drawChartStrucutre = true;
-            this.startAnimationForLineChart(new Chartist.Line('#structureChart',
-                {
-                    labels: data.structure.label,
-                    series: [
-                        data.structure.value
-                    ]
-                },
-                {
-                    lineSmooth: Chartist.Interpolation.cardinal({
-                        tension: 0
-                    }),
-                    low: 0,
-                    high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-                    chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
-                }
-            ));
-        }
+    ngOnInit() {
 
     }
 
+    getMessageError(attrName: string) {
+        return messageErrorValidation(this.form, attrName);
+    }
+
+
+    submit() {
+
+    }
 }
