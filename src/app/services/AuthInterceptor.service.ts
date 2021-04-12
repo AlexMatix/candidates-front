8a5b2cce19a6231ca6ebc89e8bf2555daa77232b
Raitoforce <mahgt_34@hotmail.com>
2021-04-11 19:25:50 -0500

    add 422 catch error on update

--- src/app/services/AuthInterceptor.service.ts
+++ src/app/services/AuthInterceptor.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {PowerService} from './power.service';
-import {ACTIVE_SYSTEM, ADMIN, DISABLE_SYSTEM} from '../util/Config.utils';
 import {OauthService} from './oauth.service';
 import {Router} from '@angular/router';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import MessagesUtil from '../util/messages.utill';
 
 @Injectable({
     providedIn: 'root'
@@ -27,6 +28,16 @@ export class AuthInterceptor implements HttpInterceptor {
                 }
             });
         }
-        return next.handle(request);
+        return next.handle(request).pipe(
+            catchError(
+                response => {
+                    if (response.status === 422) {
+                        MessagesUtil.infoMessage('Debes modificar al menos un campo para poder editar');
+                        return of(response);
+                    }
+                    return throwError(response);
+                }
+            ),
+        );
     }
 }
